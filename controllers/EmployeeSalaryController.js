63c95761b36e64c44155e8df7e58e022d17e8520
rdarshan927 <rdarshan927@gmail.com>
2024-09-23 11:07:48 +0530

    connected

--- controllers/EmployeeSalaryController.js
+++ controllers/EmployeeSalaryController.js
@@ -21,12 +21,12 @@ const createEmpoyeeSalary = async (req, res) => {
         });
 
         if(!newEmployeeSalary){
-            return res.status(400).json({message: "Failed to create the salary!"});
             console.log("Failed to create the salary!");
+            return res.status(400).json({message: "Failed to create the salary!"});
         }
 
         //send the salary as a response
-        res.json({ newEmployeeSalary });
+        res.status(200).json({ message: "Salary successfully added", newEmployeeSalary });
     }catch(error){
         res.json({message: error.message});
     }
--- routes/EmployeeSalaryRoute.js
+++ routes/EmployeeSalaryRoute.js
@@ -6,8 +6,8 @@ const router = express.Router()
 
 router.post("/employeesalary/create",EmployeeSalaryController.createEmpoyeeSalary)
 router.get("/employeesalary/getall",EmployeeSalaryController.fetchEmpoyeeSalaries)
-router.get("/employeesalary/get",EmployeeSalaryController.fetchEmpoyeeSalary)
-router.put("/employeesalary/update",EmployeeSalaryController.updateEmployeeSalary)
-router.delete("/employeesalary/delete",EmployeeSalaryController.deleteEmployeeSalary)
+router.get("/employeesalary/get/:id",EmployeeSalaryController.fetchEmpoyeeSalary)
+router.put("/employeesalary/update/:id",EmployeeSalaryController.updateEmployeeSalary)
+router.delete("/employeesalary/delete/:id",EmployeeSalaryController.deleteEmployeeSalary)
 
 module.exports = router
